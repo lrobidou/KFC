f11d7e63be94a4caeabb73f2ff8cd13309974a37
Lucas Robidou <lucas.robidou@uni-bielefeld.de>
2024-05-31 10:22:12 +0200

    create index function and SKCount type

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,9 @@ use xxhash_rust::const_xxh3::xxh3_64;
 type Minimizer = String; // TODO change to integer when done
 type HashSuperKmer = u64;
 type Count = u16;
+type SKCount = MashMap<Minimizer, (HashSuperKmer, Count)>;
+// canonical minimmizer -> (id of left hk, orientation flag), ((index of right hyperkmer, orientation flag), count)
+// orientation flag: true if the hyperkmer is in a DIFFERENT orientation that the canonical minimizer
 type HKCount = MashMap<Minimizer, ((usize, bool), (usize, bool), Count)>;
 
 mod brrr_minimizers;
@@ -17,24 +20,24 @@ use superkmers::{
     compute_superkmers_linear, get_canonical_kmer, reverse_complement, SuperKmerInfos,
 };
 
-// TODO duplication accross if/else
-fn get_count_superkmer(
-    sk_count: &MashMap<Minimizer, (HashSuperKmer, Count)>,
-    superkmer: &SuperKmerInfos,
-) -> Count {
+/// Retrieve the count of `superkmer` in `sk_count`.
+/// the superkmer and its minimizer should be canonical.
+/// Returns 0 if the superkmer is not found.
+fn get_count_superkmer(sk_count: &SKCount, superkmer: &SuperKmerInfos) -> Count {
     let superkmer_hash = xxh3_64(superkmer.superkmer.as_bytes());
     for (hash, count) in sk_count.get_mut_iter(&superkmer.minimizer) {
         if *hash == superkmer_hash {
             return *count;
         }
     }
+    // superkmer not found, count is 1
     0
 }
 
-fn update_count_superkmer(
-    sk_count: &mut MashMap<Minimizer, (HashSuperKmer, Count)>,
-    superkmer: &SuperKmerInfos,
-) -> Count {
+/// Add 1 to the count of `superkmer` in `sk_count`.
+/// If `superkmer` is not in `sk_count`, add it.
+/// Returns the new count.
+fn update_count_superkmer(sk_count: &mut SKCount, superkmer: &SuperKmerInfos) -> Count {
     let superkmer_hash = xxh3_64(superkmer.superkmer.as_bytes());
     for (super_kmer_hash, super_kmer_count) in sk_count.get_mut_iter(&superkmer.minimizer) {
         if *super_kmer_hash == superkmer_hash {
@@ -43,7 +46,7 @@ fn update_count_superkmer(
             return new_count;
         }
     }
-    // new_count is 1
+    // superkmer not found, insert it, count is 1
     sk_count.insert(superkmer.minimizer.clone(), (superkmer_hash, 1));
     1
 }
@@ -82,7 +85,7 @@ fn get_hyperkmer_right_id(
     None
 }
 
-/// Adds `new_hyperkmer` in `hyperkmers`
+/// Adds `new_hyperkmer` in `hyperkmers` and return its index
 /// `new_hyperkmer` does not have to be in canonical form
 fn add_new_hyperkmer(hyperkmers: &mut Vec<String>, new_hyperkmer: &str) -> usize {
     let id_hyperkmer = hyperkmers.len();
@@ -91,7 +94,8 @@ fn add_new_hyperkmer(hyperkmers: &mut Vec<String>, new_hyperkmer: &str) -> usize
     id_hyperkmer
 }
 
-// minimizer is already in canonical form
+/// Add a new entry in the hyperkmer counts.
+/// Minimizer should already be in canonical form.
 fn insert_new_entry_in_hyperkmer_count(
     hk_count: &mut HKCount,
     minimizer: &str,
@@ -139,53 +143,56 @@ fn get_left_and_rigth_from_hk(
     current_sk: &SuperKmerInfos,
     next_sk: &SuperKmerInfos,
 ) -> (String, String) {
-    if current_sk.was_read_canonical {
-        let start_of_minimizer_in_sk =
-            current_sk.start_of_minimizer_as_read - current_sk.start_of_superkmer_as_read;
-        assert!(current_sk.start_of_minimizer_as_read > previous_sk.start_of_minimizer_as_read);
-        let distance_to_left =
-            current_sk.start_of_minimizer_as_read - previous_sk.start_of_minimizer_as_read;
-        let start_of_left_hk = start_of_minimizer_in_sk - distance_to_left + 1;
-        let end_of_left_hk = start_of_minimizer_in_sk + current_sk.minimizer.len() - 1;
-        let left_hk = &current_sk.superkmer[start_of_left_hk..end_of_left_hk];
+    // Caution: the next and previous superkmer are given as the appear in the read.
+    // To stay consistant accross all orientation of reading,
+    // we conceptually switch the next and previous superkmers
+    // if the current superkmer was not canonical in the read.
 
-        assert!(current_sk.start_of_minimizer_as_read < next_sk.start_of_minimizer_as_read);
-        let distance_to_right =
-            next_sk.start_of_minimizer_as_read - current_sk.start_of_minimizer_as_read;
+    let (start_of_minimizer_in_sk, distance_to_left, distance_to_right) =
+        if current_sk.was_read_canonical {
+            assert!(current_sk.start_of_minimizer_as_read > previous_sk.start_of_minimizer_as_read);
+            assert!(current_sk.start_of_minimizer_as_read < next_sk.start_of_minimizer_as_read);
 
-        let start_of_right_hk = start_of_minimizer_in_sk + 1;
-        let end_of_right_hk =
-            start_of_minimizer_in_sk + distance_to_right + current_sk.minimizer.len() - 1;
-        let right_hk = &current_sk.superkmer[start_of_right_hk..end_of_right_hk];
+            let start_of_minimizer_in_sk =
+                current_sk.start_of_minimizer_as_read - current_sk.start_of_superkmer_as_read;
+            let distance_to_left =
+                current_sk.start_of_minimizer_as_read - previous_sk.start_of_minimizer_as_read;
+            let distance_to_right =
+                next_sk.start_of_minimizer_as_read - current_sk.start_of_minimizer_as_read;
+            (
+                start_of_minimizer_in_sk,
+                distance_to_left,
+                distance_to_right,
+            )
+        } else {
+            assert!(current_sk.start_of_minimizer_as_read < previous_sk.start_of_minimizer_as_read);
+            assert!(current_sk.start_of_minimizer_as_read > next_sk.start_of_minimizer_as_read);
 
-        (String::from(left_hk), String::from(right_hk))
-    } else {
-        let nb_base_after_minimizer_in_sk_when_minimizer_is_canonical =
-            current_sk.start_of_minimizer_as_read - current_sk.start_of_superkmer_as_read;
-        let start_of_minimizer_in_sk = current_sk.superkmer.len()
-            - nb_base_after_minimizer_in_sk_when_minimizer_is_canonical
-            - current_sk.minimizer.len();
-        assert!(current_sk.start_of_minimizer_as_read < previous_sk.start_of_minimizer_as_read);
-        let distance_to_left =
-            previous_sk.start_of_minimizer_as_read - current_sk.start_of_minimizer_as_read;
-        // similar
-        let start_of_left_hk = start_of_minimizer_in_sk - distance_to_left + 1;
-        let end_of_left_hk = start_of_minimizer_in_sk + current_sk.minimizer.len() - 1; // TODO test
+            let start_of_minimizer_in_sk = current_sk.superkmer.len()
+                - current_sk.start_of_minimizer_as_read
+                + current_sk.start_of_superkmer_as_read
+                - current_sk.minimizer.len();
+            let distance_to_left =
+                previous_sk.start_of_minimizer_as_read - current_sk.start_of_minimizer_as_read;
+            let distance_to_right =
+                current_sk.start_of_minimizer_as_read - next_sk.start_of_minimizer_as_read;
+            (
+                start_of_minimizer_in_sk,
+                distance_to_left,
+                distance_to_right,
+            )
+        };
 
-        let left_hk = &current_sk.superkmer[start_of_left_hk..end_of_left_hk];
+    let start_of_left_hk = start_of_minimizer_in_sk - distance_to_left + 1;
+    let end_of_left_hk = start_of_minimizer_in_sk + current_sk.minimizer.len() - 1;
+    let start_of_right_hk = start_of_minimizer_in_sk + 1;
+    let end_of_right_hk =
+        start_of_minimizer_in_sk + distance_to_right + current_sk.minimizer.len() - 1;
 
-        assert!(current_sk.start_of_minimizer_as_read > next_sk.start_of_minimizer_as_read);
-        let distance_to_right =
-            current_sk.start_of_minimizer_as_read - next_sk.start_of_minimizer_as_read;
-        // similar
-        let start_of_right_hk = start_of_minimizer_in_sk + 1; //-distance_to_right+1;
-        let end_of_right_hk =
-            start_of_minimizer_in_sk + distance_to_right + current_sk.minimizer.len() - 1; // TODO test +- 1
-        let right_hk = &current_sk.superkmer[start_of_right_hk..end_of_right_hk];
+    let left_hk = &current_sk.superkmer[start_of_left_hk..end_of_left_hk];
+    let right_hk = &current_sk.superkmer[start_of_right_hk..end_of_right_hk];
 
-        (String::from(left_hk), String::from(right_hk))
-    }
-    // }
+    (String::from(left_hk), String::from(right_hk))
 }
 
 // TODO find a better name for the first stage function
@@ -194,12 +201,8 @@ fn first_stage(
     k: usize,
     m: usize,
     threshold: Count,
-) -> (
-    MashMap<Minimizer, (HashSuperKmer, Count)>,
-    HKCount,
-    Vec<String>,
-) {
-    let mut sk_count: MashMap<Minimizer, (HashSuperKmer, Count)> = MashMap::new();
+) -> (SKCount, HKCount, Vec<String>) {
+    let mut sk_count: SKCount = MashMap::new();
     let mut hk_count: HKCount = MashMap::new();
     let mut hyperkmers: Vec<String> = Vec::new();
 
@@ -207,8 +210,8 @@ fn first_stage(
         let start_superkmers = Instant::now();
         let superkmers = compute_superkmers_linear(sequence, k, m);
         println!(
-            "super kmers computed in {} seconds",
-            start_superkmers.elapsed().as_secs()
+            "super kmers computed in {} milliseconds",
+            start_superkmers.elapsed().as_millis()
         );
 
         for triplet in superkmers.windows(3) {
@@ -231,10 +234,11 @@ fn first_stage(
                 let (left_hk, right_hk) =
                     get_left_and_rigth_from_hk(previous_sk, current_sk, next_sk);
 
-                // OK, so now left_hk and right_hk are the left and right hyperkmer
-                // as we would see them if the minimizer was in canonical foram in the read
+                // left_hk and right_hk are the left and right hyperkmer
+                // as we would see them if the minimizer was in canonical form in the read
 
                 // OPTIMIZE maybe it is posssible to call get_hyperkmer_{left, right}_id and ignore get_count_superkmer
+                // OPTIMIZE of even better: access the count of sk in streaming, so that no recomputation is needed
                 let id_left_hk = if get_count_superkmer(&sk_count, previous_sk) >= threshold {
                     if previous_sk.was_read_canonical == current_sk.was_read_canonical {
                         get_hyperkmer_right_id(
@@ -243,7 +247,7 @@ fn first_stage(
                             &previous_sk.minimizer,
                             &left_hk,
                         )
-                        .unwrap()
+                        .expect("Hash collision on superkmers. Please change your seed.")
                     } else {
                         get_hyperkmer_left_id(
                             &hk_count,
@@ -251,7 +255,7 @@ fn first_stage(
                             &previous_sk.minimizer,
                             &left_hk,
                         )
-                        .unwrap()
+                        .expect("Hash collision on superkmers. Please change your seed.")
                     }
                 } else {
                     add_new_hyperkmer(&mut hyperkmers, &left_hk)
@@ -259,7 +263,7 @@ fn first_stage(
                 let id_right_hk = if get_count_superkmer(&sk_count, next_sk) >= threshold {
                     if current_sk.was_read_canonical == next_sk.was_read_canonical {
                         get_hyperkmer_left_id(&hk_count, &hyperkmers, &next_sk.minimizer, &right_hk)
-                            .unwrap()
+                            .expect("Hash collision on superkmers. Please change your seed.")
                     } else {
                         get_hyperkmer_right_id(
                             &hk_count,
@@ -267,7 +271,7 @@ fn first_stage(
                             &next_sk.minimizer,
                             &right_hk,
                         )
-                        .unwrap()
+                        .expect("Hash collision on superkmers. Please change your seed.")
                     }
                 } else {
                     add_new_hyperkmer(&mut hyperkmers, &right_hk)
@@ -288,14 +292,6 @@ fn first_stage(
                     current_count,
                 );
             } else if current_count > threshold {
-                // left and right parts of the current superkmer
-                // truncated so that they do not include the current minimizer completely
-                // (just like hyperkmers)
-                // let left_of_sk = &sequence[current_sk.start_of_superkmer_as_read
-                //     ..=current_sk.start_of_minimizer_as_read + m - 1];
-                // let right_of_sk = &sequence[current_sk.start_of_minimizer_as_read + 1
-                //     ..current_sk.start_of_superkmer_as_read + current_sk.superkmer.len()];
-
                 let mut found = false;
                 for (id_left_hk, id_right_hk, count_hk) in
                     hk_count.get_mut_iter(&current_sk.minimizer)
@@ -328,7 +324,7 @@ fn first_stage(
 
 // TODO find a better name for the second stage function
 fn second_stage(
-    sk_count: &mut MashMap<Minimizer, (HashSuperKmer, Count)>,
+    sk_count: &mut SKCount,
     hk_count: &mut HKCount,
     hyperkmers: &[String],
     sequences: &Vec<&str>,
@@ -367,29 +363,42 @@ fn second_stage(
                 }
             }
 
-            // left hyper-k-mer of sk
-            // TODO I am taking more than the hyperkmer, but it should not be a problem since the match will stop at the hyperkmer anyway
-            // TODO canonical
             let start_of_minimizer_in_sk =
                 superkmer.start_of_minimizer_as_read - superkmer.start_of_superkmer_as_read;
             let left_hk_of_sk = &superkmer.superkmer[0..=(start_of_minimizer_in_sk + m - 1)];
             let right_hk_of_sk =
                 &superkmer.superkmer[(start_of_minimizer_in_sk + 1)..superkmer.superkmer.len()];
+
             let mut match_left = k - 1;
             let mut match_right = k - 1;
             let mut id_match_left = None;
             let mut id_match_right = None;
 
+            // Search for the maximal preffix/suffix in hyperkmers.
+            // Because we only have our left/right *partial* hyperkmer,
+            // we do not know if they are in the same orientation as their "complete" canonical form.
+            // Let's search for both orientation then.
             for (id_left_hk, id_right_hk, _count) in hk_count.get_iter(minimizer) {
-                let len_current_match_left =
-                    common_suffix_length(left_hk_of_sk, &hyperkmers[id_left_hk.0]);
+                let len_current_match_left = std::cmp::max(
+                    common_suffix_length(left_hk_of_sk, &hyperkmers[id_left_hk.0]),
+                    common_suffix_length(
+                        &reverse_complement(left_hk_of_sk),
+                        &hyperkmers[id_left_hk.0],
+                    ),
+                );
+
                 if len_current_match_left > match_left {
                     match_left = len_current_match_left;
                     id_match_left = Some(id_left_hk);
                 }
 
-                let len_current_match_right =
-                    common_prefix_length(right_hk_of_sk, &hyperkmers[id_right_hk.0]);
+                let len_current_match_right = std::cmp::max(
+                    common_prefix_length(right_hk_of_sk, &hyperkmers[id_right_hk.0]),
+                    common_prefix_length(
+                        &reverse_complement(right_hk_of_sk),
+                        &hyperkmers[id_right_hk.0],
+                    ),
+                );
                 if len_current_match_right > match_right {
                     match_right = len_current_match_right;
                     id_match_right = Some(id_right_hk);
@@ -408,6 +417,46 @@ fn second_stage(
     (truncated_hk, discarded_minimizers)
 }
 
+fn index_hyperkmers(
+    k: usize,
+    m: usize,
+    threshold: Count,
+    sequences: &Vec<&str>,
+) -> (
+    SKCount,
+    HKCount,
+    Vec<String>,
+    MashMap<String, (usize, usize)>,
+    HashMap<String, u16>,
+) {
+    let start_fisrt_step = Instant::now();
+    let (mut sk_count, mut hk_count, hyperkmers) = first_stage(sequences, k, m, threshold);
+    println!(
+        "time first stage: {} milliseconds",
+        start_fisrt_step.elapsed().as_millis()
+    );
+    let start_second_stage = Instant::now();
+    let (truncated_hk, discarded_minimizers) = second_stage(
+        &mut sk_count,
+        &mut hk_count,
+        &hyperkmers,
+        sequences,
+        k,
+        m,
+        threshold,
+    );
+    println!(
+        "time second stage: {} milliseconds",
+        start_second_stage.elapsed().as_millis()
+    );
+    (
+        sk_count,
+        hk_count,
+        hyperkmers,
+        truncated_hk,
+        discarded_minimizers,
+    )
+}
 fn main() {
     let sequences: Vec<String> = fasta_reads("data/U00096.3.fasta")
         .unwrap()
@@ -415,32 +464,12 @@ fn main() {
         .collect();
     let sequences: Vec<&str> = sequences.iter().map(|s| s.as_ref()).collect();
 
-    // println!("{:?}", sequences);
-
-    let k = 40;
+    let k = 100;
     let m = 31;
     let threshold = 2;
 
-    let start_fisrt_step = Instant::now();
-    let (mut sk_count, mut hk_count, hyperkmers) = first_stage(&sequences, k, m, threshold);
-    println!(
-        "time first step: {} seconds",
-        start_fisrt_step.elapsed().as_secs()
-    );
-    let start_second_stage = Instant::now();
-    let (truncated_hk, discarded_minimizers) = second_stage(
-        &mut sk_count,
-        &mut hk_count,
-        &hyperkmers,
-        &sequences,
-        k,
-        m,
-        threshold,
-    );
-    // println!(
-    //     "time second stage: {} seconds",
-    //     start_second_stage.elapsed().as_secs()
-    // );
+    let (_sk_count, _hk_count, _hyperkmers, _truncated_hk, _discarded_minimizers) =
+        index_hyperkmers(k, m, threshold, &sequences);
 }
 
 #[cfg(test)]
@@ -478,7 +507,7 @@ mod tests {
             start_of_superkmer_as_read: 20,
         };
 
-        let mut sk_count: MashMap<Minimizer, (HashSuperKmer, Count)> = MashMap::new();
+        let mut sk_count: SKCount = MashMap::new();
 
         for sk in vec![sk0, sk1, sk2, sk3] {
             assert_eq!(get_count_superkmer(&sk_count, &sk), 0);
