4f2aa0a2cba24bad230ea01b27ab40609a5e0d06
Lucas Robidou <lucas.robidou@uni-bielefeld.de>
2024-08-07 10:09:20 +0200

    plain text dump

--- src/dump/plain_text.rs
+++ src/dump/plain_text.rs
@@ -1,18 +1,14 @@
-use std::{fs::File, path::Path};
+use std::{fs::File, io::BufWriter, path::Path};
 
 use crate::Index;
 
 /// Dump all kmers and their abundance from the index into `output_file`.
 /// K-mers are not sorted.
 pub fn plain_text<P: AsRef<Path>>(index: &Index, output_file: P) {
-    // use std::io::Write;
-    // let mut file = File::create(output_file).unwrap();
-
-    todo!();
-    // // Iterate over the vector and write each line to the file
-    // for i in 0..ext_hyperkmers.len() {
-    //     let ext_hk = ext_hyperkmers.get_hyperkmer_from_id(i);
-    //     let line = ext_hk.to_string();
-    //     writeln!(file, "{line}").unwrap();
-    // }
+    use std::io::Write;
+    let file = File::create(output_file).unwrap();
+    let mut buffer = BufWriter::new(file);
+    for (kmer, count) in index.iter_kmers().flatten() {
+        writeln!(buffer, "{}\t{}", kmer, count).unwrap();
+    }
 }
