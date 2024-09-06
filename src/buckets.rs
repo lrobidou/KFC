27d87ecfb8e733eda2ea318b9f302fc382cbfb83
Lucas Robidou <lucas.robidou@uni-bielefeld.de>
2024-09-06 15:21:26 +0200

    add function for locking locks in order

--- src/buckets.rs
+++ src/buckets.rs
@@ -1,9 +1,9 @@
 use serde::de::{SeqAccess, Visitor};
 use serde::ser::SerializeSeq;
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
-use std::fmt;
 use std::marker::PhantomData;
-use std::sync::{Arc, RwLock};
+use std::sync::{Arc, RwLock, RwLockReadGuard, RwLockWriteGuard};
+use std::{cmp, fmt};
 
 pub const NB_BUCKETS: usize = 255;
 
@@ -108,4 +108,134 @@ impl<T: PartialEq + Serialize + for<'a> Deserialize<'a>> Buckets<T> {
     pub fn chunks(&self) -> &[Arc<RwLock<T>>; NB_BUCKETS] {
         &self.data
     }
+
+    #[cfg(test)]
+    pub fn len(&self) -> usize {
+        self.data.len()
+    }
+
+    pub fn acquire_write_locks(
+        &self,
+        idx0: u64, // current minimizer will be locked for sure
+        idx1: u64, // previous minimizer
+        idx2: u64, // next minimizer
+    ) -> (
+        RwLockWriteGuard<T>,
+        Option<RwLockReadGuard<T>>,
+        Option<RwLockReadGuard<T>>,
+        LockPosition,
+        LockPosition,
+    ) {
+        let idx0 = usize::try_from(idx0).unwrap() % NB_BUCKETS;
+        let idx1 = usize::try_from(idx1).unwrap() % NB_BUCKETS;
+        let idx2 = usize::try_from(idx2).unwrap() % NB_BUCKETS;
+
+        let mut lock_0 = None;
+        let mut lock_1 = None;
+        let mut lock_2 = None;
+
+        let mut lock_1_position = LockPosition::ThisLock;
+        let mut lock_2_position = LockPosition::ThisLock;
+
+        // Create a list of indices with their original position
+        let mut indices = [(idx0, 0), (idx1, 1), (idx2, 2)];
+
+        // Sort by index to avoid deadlocks
+        indices.sort_by_key(|&(idx, _)| idx);
+
+        // Iterate over the sorted indices and acquire the necessary locks
+        // let mut previous_val_and_pos = None;
+        for (idx, original_pos) in indices.into_iter() {
+            if idx == idx0 {
+                if lock_0.is_none() {
+                    lock_0 = Some(self.data[idx].write().unwrap());
+                }
+                if original_pos == 0 {
+                    // nothing to do
+                } else if original_pos == 1 {
+                    lock_1_position = LockPosition::CurrentLock;
+                } else {
+                    lock_2_position = LockPosition::CurrentLock;
+                }
+            } else if idx == idx1 {
+                if lock_1.is_none() {
+                    // lock_id_1 = Some(original_pos);
+                    lock_1 = Some(self.data[idx].read().unwrap());
+                }
+                if original_pos == 0 {
+                    // TODO please approve me
+                    // impossible
+                    panic!()
+                } else if original_pos == 1 {
+                    lock_1_position = LockPosition::ThisLock;
+                } else {
+                    lock_2_position = LockPosition::OtherLock;
+                }
+            } else {
+                assert!(idx == idx2);
+                assert!(lock_2.is_none());
+                if lock_2.is_none() {
+                    // lock_id_2 = Some(original_pos);
+                    lock_2 = Some(self.data[idx].read().unwrap());
+                }
+                if original_pos == 0 {
+                    panic!()
+                    // TODO please approve me
+                    // impossible
+                } else if original_pos == 1 {
+                    panic!()
+                    // TODO please approve me
+                    // impossible
+                } else {
+                    lock_2_position = LockPosition::ThisLock;
+                }
+            }
+        }
+
+        (
+            lock_0.unwrap(),
+            lock_1,
+            lock_2,
+            lock_1_position,
+            lock_2_position,
+        )
+    }
+
+    /// Acquire two locks in read mode. The first one is always acquired.
+    /// If the second one is equal to the same one, it is set to None.
+    /// The locks are sorted before TODO acquiring them.
+    /// This prevents deadlocks.
+    pub fn acquire_two_locks_read_mode(
+        &self,
+        idx0: usize, // will be locked for sure
+        idx1: usize, // might not be locked
+    ) -> (RwLockReadGuard<T>, Option<RwLockReadGuard<T>>) {
+        let idx0 = idx0 % NB_BUCKETS;
+        let idx1 = idx1 % NB_BUCKETS;
+
+        match idx0.cmp(&idx1) {
+            cmp::Ordering::Less => {
+                let lock_1 = self.data[idx0].read().unwrap();
+                let lock_2 = Some(self.data[idx1].read().unwrap());
+                (lock_1, lock_2)
+            }
+            cmp::Ordering::Equal => {
+                let lock_1 = self.data[idx0].read().unwrap();
+                let lock_2 = None;
+                (lock_1, lock_2)
+            }
+            cmp::Ordering::Greater => {
+                let lock_2 = Some(self.data[idx1].read().unwrap());
+                let lock_1 = self.data[idx0].read().unwrap();
+                (lock_1, lock_2)
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum LockPosition {
+    ThisLock,
+    CurrentLock,
+    OtherLock,
 }
