ae2552d9f4c019a7a704d17a3976f0dac1058cba
Lucas Robidou <lucas.robidou@uni-bielefeld.de>
2024-09-24 11:10:58 +0200

    add more buckets

--- src/index/components/hyperkmers_counts/hyperkmer_metadata.rs
+++ src/index/components/hyperkmers_counts/hyperkmer_metadata.rs
@@ -9,7 +9,8 @@ use serde::{Deserialize, Serialize};
 /// change_orientation: true if the hyperkmer is in a DIFFERENT orientation that the canonical minimizer it is associated with in the `HKCount` table
 #[derive(Clone, Copy, Serialize, Deserialize, PartialEq, PartialOrd, Ord, Eq)]
 pub struct HKMetadata {
-    data: HKMetadataInner<8, 24, 15, 15>,
+    // 2**32 is about 17 times lager than the largest human chomosome
+    data: HKMetadataInner<16, 46, 32, 32>,
 }
 
 impl HKMetadata {
@@ -23,24 +24,31 @@ impl HKMetadata {
         change_orientation: bool,
     ) -> Self {
         Self {
-            data: HKMetadataInner::new(bucket_id, index, start, end, is_large, change_orientation),
+            data: HKMetadataInner::new(
+                bucket_id.try_into().unwrap(),
+                index.try_into().unwrap(),
+                start.try_into().unwrap(),
+                end.try_into().unwrap(),
+                is_large,
+                change_orientation,
+            ),
         }
     }
 
     pub fn get_bucket_id(&self) -> usize {
-        self.data.get_bucket_id()
+        self.data.get_bucket_id().try_into().unwrap()
     }
 
     pub fn get_index(&self) -> usize {
-        self.data.get_index()
+        self.data.get_index().try_into().unwrap()
     }
 
     pub fn get_start(&self) -> usize {
-        self.data.get_start()
+        self.data.get_start().try_into().unwrap()
     }
 
     pub fn get_end(&self) -> usize {
-        self.data.get_end()
+        self.data.get_end().try_into().unwrap()
     }
 
     pub fn get_is_large(&self) -> bool {
@@ -77,7 +85,10 @@ struct HKMetadataInner<
     const SIZE_START: usize,
     const SIZE_END: usize,
 > {
-    data: usize,
+    // BUCKET_ID | INDEX | IS_LARGE | CHANGE_ORIENTATION
+    bucket_index_end_large: u64,
+    // START | END
+    start_stop: u64,
 }
 
 impl<
@@ -88,31 +99,36 @@ impl<
     > HKMetadataInner<SIZE_BUCKET_ID, SIZE_INDEX, SIZE_START, SIZE_END>
 {
     pub fn new(
-        bucket_id: usize,
-        index: usize,
-        start: usize,
-        end: usize,
+        bucket_id: u64,
+        index: u64,
+        start: u64,
+        end: u64,
         is_large: bool,
         // true if the hyperkmer is in a DIFFERENT orientation that the canonical minimizer it is associated with in the `HKCount` table
         change_orientation: bool,
     ) -> Self {
-        debug_assert_eq!(SIZE_BUCKET_ID + SIZE_INDEX + SIZE_START + SIZE_END + 2, 64);
-        assert!(bucket_id < 2usize.pow((SIZE_BUCKET_ID + 1) as u32));
-        assert!(index < 2usize.pow((SIZE_INDEX + 1) as u32));
-        assert!(start < 2usize.pow((SIZE_START + 1) as u32));
-        assert!(end < 2usize.pow((SIZE_END + 1) as u32));
-        let data = bucket_id << (SIZE_INDEX + SIZE_START + SIZE_END + 2);
-        let data = data + (index << (SIZE_BUCKET_ID + SIZE_START + SIZE_END + 2) >> SIZE_BUCKET_ID);
-        let data = data
-            + ((start << (SIZE_BUCKET_ID + SIZE_INDEX + SIZE_END + 2))
-                >> (SIZE_BUCKET_ID + SIZE_INDEX));
-        let data = data
-            + ((end << (SIZE_BUCKET_ID + SIZE_INDEX + SIZE_START + 2))
-                >> (SIZE_BUCKET_ID + SIZE_INDEX + SIZE_START));
-        let data = data + 2 * (is_large as usize);
-        let data = data + change_orientation as usize;
+        // check that the sum of size is equal to 64
+        debug_assert_eq!(SIZE_BUCKET_ID + SIZE_INDEX + 2, 64);
+        debug_assert_eq!(SIZE_START + SIZE_END, 64);
+        // check that each field is below the limit
+        assert!(bucket_id < 2usize.pow((SIZE_BUCKET_ID + 1) as u32) as u64);
+        assert!(index < 2usize.pow((SIZE_INDEX + 1) as u32) as u64);
+        assert!(start < 2usize.pow((SIZE_START + 1) as u32) as u64);
+        assert!(end < 2usize.pow((SIZE_END + 1) as u32) as u64);
 
-        let me = Self { data };
+        let bucket_index_end_large = bucket_id << (SIZE_INDEX + 2);
+        let bucket_index_end_large =
+            bucket_index_end_large + (index << (SIZE_BUCKET_ID + 2) >> SIZE_BUCKET_ID);
+        let bucket_index_end_large = bucket_index_end_large + 2 * (is_large as u64);
+        let bucket_index_end_large = bucket_index_end_large + change_orientation as u64;
+
+        let start_stop = start << SIZE_END;
+        let start_stop = start_stop + ((end << SIZE_START) >> (SIZE_START));
+
+        let me = Self {
+            bucket_index_end_large,
+            start_stop,
+        };
 
         debug_assert_eq!(me.get_bucket_id(), bucket_id);
         debug_assert_eq!(me.get_index(), index);
@@ -124,37 +140,35 @@ impl<
         me
     }
 
-    pub fn get_bucket_id(&self) -> usize {
+    pub fn get_bucket_id(&self) -> u64 {
         // first N bits
         let shift_amount = 64 - SIZE_BUCKET_ID;
-        self.data >> shift_amount
+        self.bucket_index_end_large >> shift_amount
     }
 
-    pub fn get_index(&self) -> usize {
+    pub fn get_index(&self) -> u64 {
         let nb_bits_up = SIZE_BUCKET_ID;
-        let nb_bits_down = SIZE_START + SIZE_END + 2;
-        (self.data << nb_bits_up) >> (nb_bits_up + nb_bits_down)
-    }
-
-    pub fn get_start(&self) -> usize {
-        let nb_bits_up = SIZE_BUCKET_ID + SIZE_INDEX;
-        let nb_bits_down = SIZE_END + 2;
-        (self.data << nb_bits_up) >> (nb_bits_up + nb_bits_down)
-    }
-
-    pub fn get_end(&self) -> usize {
-        let nb_bits_up = SIZE_BUCKET_ID + SIZE_INDEX + SIZE_START;
         let nb_bits_down = 2;
-        (self.data << nb_bits_up) >> (nb_bits_up + nb_bits_down)
+        (self.bucket_index_end_large << nb_bits_up) >> (nb_bits_up + nb_bits_down)
+    }
+
+    pub fn get_start(&self) -> u64 {
+        let nb_bits_down = SIZE_END;
+        self.start_stop >> nb_bits_down
+    }
+
+    pub fn get_end(&self) -> u64 {
+        let nb_bits_up = SIZE_START;
+        (self.start_stop << nb_bits_up) >> nb_bits_up
     }
 
     pub fn get_is_large(&self) -> bool {
-        (self.data >> 1) % 2 == 1
+        (self.bucket_index_end_large >> 1) % 2 == 1
     }
 
     pub fn get_change_orientation(&self) -> bool {
         // last bit of the data
-        self.data % 2 == 1
+        self.bucket_index_end_large % 2 == 1
     }
 }
 
@@ -173,7 +187,7 @@ mod tests {
         let change_orientation = false;
 
         // creation of the structure
-        let hk_metadata_inner = HKMetadataInner::<8, 24, 15, 15>::new(
+        let hk_metadata_inner = HKMetadataInner::<16, 46, 32, 32>::new(
             bucket_id,
             index,
             start,
@@ -204,7 +218,7 @@ mod tests {
         let change_orientation = false;
 
         // creation of the structure
-        let hk_metadata_inner = HKMetadataInner::<8, 24, 15, 15>::new(
+        let hk_metadata_inner = HKMetadataInner::<16, 46, 32, 32>::new(
             bucket_id,
             index,
             start,
@@ -228,15 +242,15 @@ mod tests {
     #[test]
     fn test_hyper_kmer_metadata_inner_all_max() {
         // parameters
-        let bucket_id = 255;
-        let index = 16777215;
-        let start = 32767;
-        let end = 32767;
+        let bucket_id = 65536;
+        let index = 70368744177664;
+        let start = 4294967296;
+        let end = 4294967296;
         let is_large = true;
         let change_orientation = true;
 
         // creation of the structure
-        let hk_metadata_inner = HKMetadataInner::<8, 24, 15, 15>::new(
+        let hk_metadata_inner = HKMetadataInner::<16, 46, 32, 32>::new(
             bucket_id,
             index,
             start,
